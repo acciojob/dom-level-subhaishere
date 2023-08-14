701a3489c4370a8e1e0714d12cbaf5e616cfcd2e
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 10:10:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+let targetElement = document.getElementById("level");
+
+function getDomLevel(element) {
+    let level = 0;
+    let currentElement = element;
+
+    while (currentElement.parentElement) {
+        level++;
+        currentElement = currentElement.parentElement;
+    }
+
+    return level;
+}
+
+let domLevel = getDomLevel(targetElement);
+
+alert(`The level of the element is: ${domLevel}`);
